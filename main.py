1abebdb1817d271518ada1cec5723b8429f3e49d
Brian G <brigeo987@gmail.com>
2026-02-09 21:57:02 -0500

    comments

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+# CIS 2131 Line Length Analyzer
+
+# ask for a file to read
 fname = input('Enter a file name: ')
+# ensure the file exists
 try:
     fhand = open(fname)
 except:
     print("File not found. Please create it or search for another file.")
-    exit()
+    exit() # exit if no file exists
+# set variables
 count = 0
 largest = 0
 longest_line = ""
+# loop through the file
 for line in fhand:
+    # strip each line
     stripped = line.strip()
+    # check the length, over 40 characters long and it counts up
     if len(stripped) > 40:
         count += 1
+    # find the largest line
     if largest == 0 or len(stripped) > largest:
         largest = len(stripped)
         longest_line = line
+# print the results
 print(f'Line: {line}\nCount: {count}\nLargest: {largest}\nLongest Line: {longest_line}')
 print(str(stripped))
